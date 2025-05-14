f8616064e328460d59346ca4cc301ae102b16d8c
Juan Lira <juancassiano@hotmail.com>
2025-05-14 20:53:32 -0300

    feat: create GET allUsers method

--- src/main/java/com/github/juancassiano/quarkussocial/rest/UserResource.java
+++ src/main/java/com/github/juancassiano/quarkussocial/rest/UserResource.java
@@ -2,15 +2,26 @@ package com.github.juancassiano.quarkussocial.rest;
 
 import com.github.juancassiano.quarkussocial.rest.dto.CreateUserRequest;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
   
   @POST
   public Response createUser(CreateUserRequest createUserResquest){
+    return Response.ok(createUserResquest).build();
+  }
+
+  @GET
+  public Response listAllUsers(){
     return Response.ok().build();
   }
 }
