afe5b5ff3240dfeabf5662f45d4801037d180b19
Juan Lira <juancassiano@hotmail.com>
2025-05-15 00:22:18 -0300

    feat: create User Entity

---
+++ src/main/java/com/github/juancassiano/quarkussocial/domain/model/User.java
@@ -0,0 +1,67 @@
+package com.github.juancassiano.quarkussocial.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+  
+  @Column(nullable = false)
+  private Integer age;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    User user = (User) o;
+
+    if (!id.equals(user.id)) return false;
+    if (!name.equals(user.name)) return false;
+    return age.equals(user.age);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id.hashCode();
+    result = 31 * result + name.hashCode();
+    result = 31 * result + age.hashCode();
+    return result;
+  }
+
+}
