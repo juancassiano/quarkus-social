5e42fa1fa799c7976e62a39fe1021bed193d59a4
Juan Lira <juancassiano@hotmail.com>
2025-05-17 20:27:20 -0300

    feat: create all crud operations

--- src/main/java/com/github/juancassiano/quarkussocial/rest/UserResource.java
+++ src/main/java/com/github/juancassiano/quarkussocial/rest/UserResource.java
@@ -6,9 +6,12 @@ import com.github.juancassiano.quarkussocial.rest.dto.CreateUserRequest;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -35,4 +38,33 @@ public class UserResource {
     PanacheQuery<User> query = User.findAll();
     return Response.ok(query.list()).build();
   }
+
+  @DELETE
+  @Transactional
+  @Path("/{id}")
+  public Response deleteUser(@PathParam("id") Long id){
+    User user = User.findById(id);
+    if (user == null) {
+      return Response.status(Response.Status.NOT_FOUND).build();
+    }
+    user.delete();
+    return Response.noContent().build();
+    
+  }
+
+  @PUT
+  @Path("/{id}")
+  @Transactional
+  public Response updateUser(@PathParam("id") Long id, CreateUserRequest createUserRequest){
+    User user = User.findById(id);
+    if (user == null) {
+      return Response.status(Response.Status.NOT_FOUND).build();
+    }
+    user.setName(createUserRequest.getName());
+    user.setAge(createUserRequest.getAge());
+    user.persist();
+    return Response.ok(user).build();
+  }
 }
+
+
