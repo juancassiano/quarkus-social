01cc71b0f35feb75d702fe027b89d1e2d88627a4
Juan Lira <juancassiano@hotmail.com>
2025-05-17 20:45:21 -0300

    feat: change PanacheEntityBase to Repository pattern

--- src/main/java/com/github/juancassiano/quarkussocial/domain/model/User.java
+++ src/main/java/com/github/juancassiano/quarkussocial/domain/model/User.java
@@ -1,6 +1,5 @@
 package com.github.juancassiano.quarkussocial.domain.model;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
